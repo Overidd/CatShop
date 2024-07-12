5714efcb83a964512a5ab6f5225fd52322cdae2a
John <johnelvis963@gmail.com>
2024-07-12 17:34:27 -0500

    feat: solucion del menu del manejo de stiky

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
    return (
       <html lang="en">
-         <body className={cn(roboto.className, 'text-textPrimary bg-bgSecondary overflow-x-hidden')}>
+         <body className={cn(roboto.className, 'text-textPrimary bg-bgSecondary ')}>
             <ModeDark>
                <ProductsProvider>
 
